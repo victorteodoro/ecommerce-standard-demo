13f9205b1b065bfd59b05aedd4d9919614ebe65c
Fabrizzio Fazenda <fabrizzio.fs@gmail.com>
2018-04-03 19:32:23 -0300

    Update index.js

--- src/containers/SignatureScreen/adm/NewClient/index.js
+++ src/containers/SignatureScreen/adm/NewClient/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 import {
   Header,
   Footer,
   NewSignatureMundiForm
 } from '../../../../components';
-
-// Importing components
-// import UserCardsArea from '../../../UserCardsArea/';
-// import CardsArea from '../../../CardsArea/';
-
-// Importing assets
-// import inputs from '../../../../resources/SignatureScreen/inputs';
 
 // Importing styles
 import styles from './styles.css';
