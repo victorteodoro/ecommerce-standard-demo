83b30b2f02318de53751495d2a1c2ab7379b8ba1
fabrifs <fabrizzio.fs@gmail.com>
2018-04-15 15:50:01 -0300

    fix: header links

--- src/containers/LandScreen/index.js
+++ src/containers/LandScreen/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import {
   Header,
   Footer,
-  CartProductsNew,
-  PurchaseSummaryNew
 } from '../../components/';
 
 import styles from './styles.css';
